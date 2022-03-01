1ca85101df9692807872a0e99c724ee25854426c
lbrkov00 <82526729+lbrkov00@users.noreply.github.com>
2022-03-01 11:17:03 +0100

    update CreateUser function

--- EvOil.Ui/MainWindow.xaml.cs
+++ EvOil.Ui/MainWindow.xaml.cs
@@ -7,9 +7,6 @@ using System;
 
 namespace EvOil.Ui;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public readonly UserService _databaseContextUser;
